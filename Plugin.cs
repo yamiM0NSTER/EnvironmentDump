d75bfc50d53caeeb0f6c80ff6714fca9ca84f25d
yamiM0NSTER <jomex50@gmail.com>
2021-12-20 01:27:05 +0100

    Init

---
+++ Plugin.cs
@@ -0,0 +1,55 @@
+﻿using IPA;
+using IPA.Config;
+using IPA.Utilities;
+using IPA.Config.Stores;
+using UnityEngine.SceneManagement;
+using IPA.Logging;
+using HarmonyLib;
+using System;
+using System.Collections;
+
+namespace EnvironmentDump {
+  [Plugin(RuntimeOptions.SingleStartInit)]
+  public class Plugin {
+    public static string PluginName => "EnvironmentDump";
+
+    public static Logger log { get; private set; }
+
+    [Init]
+    public void Init(Logger logger, Config cfgProvider) {
+      log = logger;
+    }
+
+    [OnStart]
+    public void OnApplicationStart() {
+      log.Info("Environment Variables:");
+      foreach(DictionaryEntry entry in Environment.GetEnvironmentVariables()) {
+        log.Info($" {entry.Key}:{entry.Value}");
+      }
+    }
+
+    [OnExit]
+    public void OnApplicationQuit() {
+    }
+
+    public void OnFixedUpdate() {
+
+    }
+
+    public void OnUpdate() {
+
+    }
+
+    public void OnActiveSceneChanged(Scene prevScene, Scene nextScene) {
+
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode) {
+
+    }
+
+    public void OnSceneUnloaded(Scene scene) {
+
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# EnvironmentDump
+
+Simple plugin to dump environment variables on start for debugging purposes. If someone asks you to give them logs with output of this plugin make sure you are not leaking private information.
+
+# Prequisities
+
+BSIPA 4.2.0 or higher
+Visual Studio 2022 if you want to change the plugin
